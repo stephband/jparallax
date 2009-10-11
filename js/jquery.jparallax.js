3d8b944c1110fa1f9a818fd351414585f09b56c0
stephband <stephband@stephband.local>
2009-10-11 02:34:39 +0200

    Adds local.mouse handling before global.mouse. Adds proper enter/leave mouseport handling. Re-enables soft takeover via ontarget flag. Adds freeze-to-position event handling.

--- js/jquery.jparallax.js
+++ js/jquery.jparallax.js
@@ -26,177 +26,248 @@ var undefined,
         ytravel:                1,                      //
         takeoverDecay:          0.66,                   // Sets rate of decay curve for catching up with target mouse position
         takeoverThresh:         0.002,                  // Sets the distance within which virtualmouse is considered to be on target, as a multiple of mouseport width
-        frameDuration:          25,                     // In milliseconds
-        layerFreezeClass:       'freeze'                // Class added to layer when frozen
+        frameDuration:          30,                     // In milliseconds
+        freezeClass:            'freeze'                // Class added to layer when frozen
     },
     value = {left: 0, top: 0, middle: 0.5, center: 0.5, centre: 0.5, right: 1, bottom: 1},
     abs = Math.abs,
     regex = {
         px:         /^\d+\s?px$/,
-        percent:    /^\d+\s?%$/,
-        html:       /\.html?\s?$/,
-        hash:       /^#/
+        percent:    /^\d+\s?%$/
     },
-    pointer = [],
-    Mouse = function(options){
-        this.ontarget   = false;
-        this.thresh     = options.takeoverThresh || 0.01;
-        this.decay      = options.takeoverDecay || 0.666;
-        this.pos        = [0.5, 0.5];
-        this.updatePos  = function(portSize, pointerPos){
-            var pos = [], x = 2;
-            
-            // Calculate position as ratio
-            while (x--) { pos[x] = pointerPos[x] / portSize[x]; }
-            
-            if (!this.ontarget) {
-                if ( distance(this.pos, pos ) < this.thresh) {
-                    // Lock mouse to pointer
-                    this.ontarget = true;
-                }
-                else {
-                    // Follow pointer with mouse, lagging according to decay
-                    x = 2; while (x--) { pos[x] = pos[x] + this.decay * (this.pos[x] - pos[x]); }
-                };
-            }
-            this.pos = pos;
-            return pos;
-        };
-    },
-    Port = function(elem, options){
-        var self = this;
-        this.elem = elem;
-        this.updateSize = function(){
-            self.size = [self.elem.width(), self.elem.height()];
-        };
-        this.updatePos = function(){
-            var elem = self.elem;
-                offset = elem.offset() || {left: 0, top: 0};
-            
-            this.pos = [offset.left, offset.top];
-        };
-        this.activeOutside = (options && options.activeOutside) || false;
-        this.active = function(pointer){
-            var pos = this.pos,
-                size = this.size,
-                activeOutside = this.activeOutside,
-                inside = [],
-                x;
-            // Is mouse inside port?
-            
-            // Method 1 (uses pointer position calculations)
-            // x = 2; while (x--) {
-            //     var d = pointer[x] - pos[x];
-            //     if (activeOutside || d > 0 && d < size[x]) { inside[x] = d; }
-            //     // No
-            //     else { return false; }
-            // }
-            // // Yes
-            // this.pointer = inside;
-            // return inside;
-            
-            // Method 2 (uses flags from mouseenter and mouseleave events)
-            // Yes.
-            if (activeOutside || this.inside > 0) {
-                x = 2; while (x--) {
-                    inside[x] = pointer[x] - pos[x];
-                }
-                this.pointer = inside;
-                return inside;
-            }
-            // No, but it only just went outside so make one last move.
-            if (this.inside < 0) {
-                this.inside = 0;
-                return this.pointer;
-            }
-            // No.
-            return false;
-        };
-        this.enter = function(e){
-            var port = e.data.port;
-            
-            port.inside = 1;
-            
-            //port.pointer = [e.pageX - port.pos[0], e.pageY - port.pos[1]];
-        };
-        this.leave = function(e){
-            var port = e.data.port,
-                x = 2;
-            
-            port.inside = -1;
-            
-            // TODO: If port is not a DOM element, mouseleave coords (e.pageX and e.pageY) are really spurious in Safari.  
-            port.pointer = [e.pageX - port.pos[0], e.pageY - port.pos[1]];
-            
-            // Truncate exit pos at port boundaries
-            while (x--) { port.pointer[x] = port.pointer[x] < 0 ? 0 : port.pointer[x] > port.size[x] ? port.size[x] : port.pointer[x] ; }
-            
-            //console.log(port.pointer);            
-        };
-        
-        jQuery(window)
-        .bind('resize', this.updateSize)
-        .bind('resize', this.updatePos);
-        
-        elem
-        .bind('mouseenter', {port: this}, this.enter)
-        .bind('mouseleave', {port: this}, this.leave);
-        
-        this.updateSize();
-        this.updatePos();
-    },
-    Layer = function(elem, options){
-        this.elem   = elem;
-        this.size   = options.size || [elem.width(), elem.height()];
-        this.trav   = options.travel || [1, 1];
-        this.trpx   = options.travelpx || [false, false];
-        this.offset = [0, 0];
-        this.pos    = [];
-        this.updatePos = function(mousePos){
-            var pos = [], x;
-            x = 2; while (x--) { pos[x] = this.trav[x] * mousePos[x] + this.offset[x]; }
-            this.pos = pos;
-            return pos;
-        };
-        this.updateCss = function(){
-            var pos = this.pos,
-                size = this.size,
-                trpx = this.trpx,
-                css =  {
-                    left:       (trpx[0]) ? undefined : pos[0] * 100 + '%' ,
-                    marginLeft: (trpx[0]) ? pos[0] * -1 + 'px' : pos[0] * size[0] * -1 + 'px' ,
-                    top:        (trpx[1]) ? undefined : pos[1] * 100 + '%' ,
-                    marginTop:  (trpx[1]) ? pos[1] * -1 + 'px' : pos[1] * size[1] * -1 + 'px'
-                };
-            this.elem.css(css);
-            return css;
-        };
-    },
+    pointer = [0.5, 0.5],
+    // Event handlers
     actions = {
         freeze: function(e){
-            jQuery(this).unbind('frame.'+plugin);
-            return false;
+            var elem = jQuery(this),
+                local = elem.data(plugin),
+                global = e.data,
+                mouse = local.mouse || local.freeze || global.mouse,
+                x = regex.percent.exec(e.x) ? parseFloat(e.x.replace(/%$/, ''))/100 : (e.x || mouse.pointer[0]) ,
+                y = regex.percent.exec(e.y) ? parseFloat(e.y.replace(/%$/, ''))/100 : (e.y || mouse.pointer[1]) ;
+            
+            // Store position, copying mouse pointer
+            local.freeze = {
+                pointer: [x, y]
+            }
+            
+            // Create local mouse, passing in current pointer with options
+            global.pointer = mouse.pointer;
+            local.mouse = new Mouse(global);
+                
+            elem
+            .bind('frame.'+plugin, global, actions.update);
         },
         unfreeze: function(e){
-            // Start animating layer, passing options in as e.data
-            // Special event 'frame' will pick up and use frameDuration
-            jQuery(this).bind('frame.'+plugin, e.data, actions.update);
-            return false;
+            var elem = jQuery(this),
+                local = elem.data(plugin),
+                global = e.data;
+            
+            if (local.freeze) {
+                // Create local mouse, passing local freeze pointer with options
+                global.pointer = local.mouse ? local.mouse.pointer : local.freeze.pointer ;
+                local.mouse = new Mouse(global);
+                
+                // Destroy local.freeze
+                local.freeze = null;
+                
+                // Animate
+                elem
+                .removeClass(options.freezeClass)
+                .bind('frame.'+plugin, global, actions.update);
+            }
+        },
+        activate: function(e){
+            var global = e.data;
+            
+            global.mouse.ontarget = false;
+            
+            // Animate unfrozen layers
+            this
+            .not('.'+global.freezeClass)
+            .bind('frame.'+plugin, global, actions.update);
         },
         update: function(e){
-            var data = jQuery(this).data(plugin),
-                options = e.data,
-                layer = data.layer,
-                mouse = data.mouse,
-                port = options.port;
+            var elem = jQuery(this),
+                global = e.data,
+                local = elem.data(plugin),
+                port = global.port,
+                mouse = local.mouse;
             
-            if (port.active(pointer)) {
-                mouse.updatePos(port.size, port.pointer);
-                layer.updatePos(mouse.pos);
-                layer.updateCss();
-            };
+            // Layer has it's own mouse
+            if ( mouse ) {
+                // Process mouse and destroy it when it hits target
+                if ( mouse.update( local.freeze ? local.freeze.pointer : port.pointer ) ) {
+                    local.mouse = null;
+                    if (local.freeze) {
+                        elem
+                        .unbind('frame.'+plugin)
+                        .addClass(global.freezeClass);
+                    }
+                }
+            }
+            // Layer responds to global mouse
+            else {
+                mouse = global.mouse;
+                
+                // Only process global objects once per frame
+                if ( global.timeStamp !== e.timeStamp ) {
+                    global.timeStamp = e.timeStamp;
+                    
+                    // console.log('Frame: '+e.frameCount);
+                    
+                    // The port is active
+                    if ( port.update(pointer) || !mouse.ontarget ) {
+                        mouse.update(port.pointer);
+                    }
+                }
+                
+                // When no longer active, unbind
+                if ( mouse.ontarget && !port.active ) {
+                    elem.unbind('frame.'+plugin);
+                }
+            }
+            
+            local.layer.update(mouse.pointer);
+            
+            return false;
         }
     };
+
+// CONSTRUCTORS
+
+function Mouse(options){
+    this.ontarget = false;
+    this.thresh = options.takeoverThresh || 0.01;
+    this.decay = options.takeoverDecay || 0.666;
+    this.pointer = options.pointer || [0, 0];
+    
+    this.update = function(pointer){
+        var decay = this.decay,
+        
+        // Pointer is already on target
+        if (this.ontarget) {
+            this.pointer = pointer;
+        }
+        // Pointer has arrived within the target threshold
+        else if ( distance(this.pointer, pointer) < this.thresh ) {
+            this.ontarget = true;
+            // console.log('ONTARGET');
+            this.pointer = pointer;
+        }
+        // Pointer is nowhere near the target
+        else {
+            var lagPointer = [],
+                x = 2;
+            while (x--) {
+                lagPointer[x] = pointer[x] + decay * (this.pointer[x] - pointer[x]);
+            }
+            this.pointer = lagPointer;
+        }
+        
+        return this.ontarget;
+    };
+}
+    
+function Port(elem, options){
+    var self = this,
+        inside = 0,
+        leaveCoords;
+    
+    this.elem = elem;
+    this.updateSize = function(){
+        self.size = [self.elem.width(), self.elem.height()];
+    };
+    this.updatePos = function(){
+        var elem = self.elem,
+            offset = elem.offset() || {left: 0, top: 0};
+        
+        self.pos = [offset.left, offset.top];
+    };
+    this.pointer = options.pointer || [0, 0],
+    this.active = false,
+    this.activeOutside = (options && options.activeOutside) || false;
+    this.update = function(coords){
+        var pos = this.pos,
+            size = this.size,
+            pointer = [],
+            x = 2;
+        
+        // Is mouse inside port?
+        // Yes. Calculate pointer as ratio.
+        if ( inside > 0 ) {
+            while (x--) {
+                pointer[x] = (coords[x] - pos[x]) / size[x];
+            }
+            this.pointer = pointer;
+            this.active = true;
+            return true;
+        }
+        // No, but it only just went outside, so make one last move.
+        if ( inside < 0 ) {
+            this.inside = 0;
+            this.active = true;
+            
+            // Get coords stored by mouseleave event
+            if (leaveCoords) coords = leaveCoords;
+            
+            // Truncate pointer at port boundary
+            while (x--) {
+                pointer[x] = (coords[x] - pos[x]) / size[x];
+                pointer[x] = pointer[x] < 0 ? 0 : pointer[x] > 1 ? 1 : pointer[x] ;
+            }
+            this.pointer = pointer;
+            return true;
+        }
+        // No.
+        this.active = false;
+        return false;
+    };
+    
+    jQuery(window)
+    .bind('resize', self.updateSize)
+    .bind('resize', self.updatePos);
+    
+    elem
+    .bind('mouseenter', function(e){
+        inside = 1;
+    })
+    .bind('mouseleave', function(e){
+        inside = -1;
+        leaveCoords = [e.pageX, e.pageY];
+    });
+    
+    this.updateSize();
+    this.updatePos();
+}
+
+function Layer(elem, options){
+    this.elem   = elem;
+    this.size   = options.size || [elem.width(), elem.height()];
+    this.trav   = options.travel || [1, 1];
+    this.trpx   = options.travelpx || [false, false];
+    this.offset = [0, 0];
+    this.update = function(pointer){
+        var pos = [],
+            size = this.size,
+            trpx = this.trpx,
+            css,
+            x = 2;
+        
+        while (x--) {
+            pos[x] = this.trav[x] * pointer[x] + this.offset[x];
+        }
+        
+        css =  {
+            left:       (trpx[0]) ? undefined : pos[0] * 100 + '%' ,
+            marginLeft: (trpx[0]) ? pos[0] * -1 + 'px' : pos[0] * size[0] * -1 + 'px' ,
+            top:        (trpx[1]) ? undefined : pos[1] * 100 + '%' ,
+            marginTop:  (trpx[1]) ? pos[1] * -1 + 'px' : pos[1] * size[1] * -1 + 'px'
+        };
+        
+        this.elem.css(css);
+    };
+}
 
 // FUNCTIONS
 
@@ -222,33 +293,35 @@ parseValue.lib = value;
 
 jQuery.fn[plugin] = function(o){
     
-    var options = jQuery.extend({}, jQuery.fn[plugin].options, o);
+    var global = jQuery.extend({}, jQuery.fn[plugin].options, o),
+        layers = this,
+        port = new Port(global.mouseport, global),
+        mouse = new Mouse(global);
     
-    var port = new Port(options.mouseport, options);
-    var mouse = new Mouse(options);
+    global.port = port;
+    global.mouse = mouse;
     
-    options.port = port;
+    global.mouseport
+    .bind("mouseenter", global, actions.activate, layers);
     
-    return this.each(function(){
+    return layers
+    .bind("freeze", global, actions.freeze)
+    .bind("unfreeze", global, actions.unfreeze)
+    .each(function(){
+        var layer = jQuery(this),
+            layerData = new Layer(layer, global);
         
-        var layer = new Layer(jQuery(this), options);
-        
-        jQuery(this).data(plugin, {
-            layer: layer,
-            mouse: mouse
+        layer.data(plugin, {
+            layer: layerData
         });
-        
-    })
-    .bind("freeze", options, actions.freeze)
-    .bind("unfreeze", options, actions.unfreeze)
-    .trigger("unfreeze");
+    });
 }
 
-// RUN
+// EXPOSE
 
 jQuery.fn[plugin].options = options;
 
-// READY
+// RUN
 
 jQuery(document).ready(function(){
     jQuery(window)
